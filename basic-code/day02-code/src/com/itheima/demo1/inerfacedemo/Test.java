7f93551863deb59eab38dbb9b10859af72b896e9
zxl <zxl17339811909@163.com>
2024-06-02 21:56:56 +0800

    demo

--- basic-code/day02-code/src/com/itheima/demo1/inerfacedemo/Test.java
+++ basic-code/day02-code/src/com/itheima/demo1/inerfacedemo/Test.java
@@ -1,7 +1,9 @@
 package com.itheima.demo1.inerfacedemo;
 
+import java.io.IOException;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //创建青蛙的对象
         Frog f = new Frog("小青",1);
         System.out.println(f.getName() + ", " + f.getAge());
@@ -9,5 +11,15 @@ public class Test {
         f.swim();
         f.show();
         Swim.moo();
+        System.out.println(System.currentTimeMillis());
+        // 运行CMD命令
+        //shutdown:关机
+        //加上参数才可以执行
+        //-s 默认在1分钟之后关机
+        //-s -t 指定之间： 关机时间
+        //-a: 取消关机
+        //-r： 关机并重启
+        Runtime.getRuntime().exec("shutdown -s -t 3600");
+//        Runtime.getRuntime().exec("shutdown -a");
     }
 }

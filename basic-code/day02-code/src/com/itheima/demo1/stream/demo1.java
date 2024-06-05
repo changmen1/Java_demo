668066048fd4dd0a132d36a62ab7a1de8c932b8f
zxl <zxl17339811909@163.com>
2024-06-05 21:08:18 +0800

    demo

---
+++ basic-code/day02-code/src/com/itheima/demo1/stream/demo1.java
@@ -0,0 +1,15 @@
+package com.itheima.demo1.stream;
+
+import java.util.ArrayList;
+
+public class demo1 {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("张无忌");
+        list1.add("周芷若");
+        list1.add("赵敏");
+        list1.add("张强");
+        list1.add("张三丰");
+        list1.stream().filter(name->name.startsWith("张")).filter(name->name.length() == 3).forEach(name->System.out.println(name));
+    }
+}
